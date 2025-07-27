af8459b25732c3c1c469e02186819ed424a9b206
sijan2006 <sijansap11@gmail.com>
2025-07-27 15:52:08 +0545

    version8.1.7.2

--- csit/C++/lab8/program7.cpp
+++ csit/C++/lab8/program7.cpp
@@ -1,1 +1,46 @@
 // WAP that shows dynamic objects.
+#include <iostream>
+using namespace std;
+
+class Demo
+{
+private:
+    int value;
+
+public:
+    Demo()
+    {
+        value = 0;
+        cout << "Default Constructor. Value: " << value << endl;
+    }
+
+    Demo(int v)
+    {
+        value = v;
+        cout << "Parameterized Constructor. Value: " << value << endl;
+    }
+
+    void Display()
+    {
+        cout << "Value: " << value << endl;
+    }
+
+    ~Demo()
+    {
+        cout << "Destructor Called. Value was: " << value << endl;
+    }
+};
+
+int main()
+{
+    Demo *obj1 = new Demo;
+    Demo *obj2 = new Demo(50);
+
+    obj1->Display();
+    obj2->Display();
+
+    delete obj1;
+    delete obj2;
+
+    return 0;
+}
